c233188e748650208d507fccb7666c0946ab33eb
3bdulra7manAmir <ethanhunt4d@gmail.com>
2023-07-18 20:48:47 +0300

    DB error

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:sqlite/NavBarScreens/archived_tasks.dart';
 import 'package:sqlite/NavBarScreens/tasks.dart';
-
 import '../NavBarScreens/done_tasks.dart';
 
 class HomeLayout extends StatefulWidget {
@@ -19,6 +19,14 @@ int currentIndex = 0;
 List<Text> appbarText = const [Text("New Tasks"), Text("Done Tasks"), Text("Archived Tasks")];
 //List<String> titles = ["New Tasks","Done Tasks","Archived Tasks"];
 List<Widget> screens = const [Tasks(), DoneTasks(), ArchivedTasks()];
+
+late Database database;
+
+@override
+  void initState() {
+    createDatabase();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +56,7 @@ List<Widget> screens = const [Tasks(), DoneTasks(), ArchivedTasks()];
           //   print("error $error");
           // });
 
-          
+          insertToDatabase();
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -80,5 +88,43 @@ List<Widget> screens = const [Tasks(), DoneTasks(), ArchivedTasks()];
 // Future<String> printName() async{
 //   return "Ahmed Ali";
 // }
+
+void createDatabase() async{
+
+await openDatabase(
+  "todo.db",
+  version: 1,
+  onCreate: (database, version){
+    print("database Created");
+
+    database.execute('CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)').then((value){
+      print("Table Created");
+    }).catchError((error)
+    {
+      print("Error While Creating Database: $error");
+    });
+
+  },
+  onOpen: (database){
+    print("database Opened");
+  },
+  );
+  
+}
+
+void insertToDatabase(){
+  database.transaction((txn){
+    txn.rawInsert('INSERT INTO tasks(title, date, time, status) VALUES("First Task","02222","891","new")').
+    then((value){
+      print("$value Inserted Successfully");
+    }).catchError((error){
+      print("Error while Inserting: $error");
+    });
+    throw("error"); //me
+  },
+  );
+}
+
+
 
 }
