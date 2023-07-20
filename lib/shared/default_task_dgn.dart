d21c47a4cb38355187df956d47f7a17fbca4f9ae
3bdulra7manAmir <ethanhunt4d@gmail.com>
2023-07-20 16:30:13 +0300

    change

--- lib/shared/default_task_dgn.dart
+++ lib/shared/default_task_dgn.dart
@@ -7,18 +7,16 @@ Padding(
       padding: const EdgeInsets.all(15.0),
       child: Row(
         children: [
-          const CircleAvatar(
+          CircleAvatar(
             radius: 40,
-            child: Text("02:00 PM"),
+            child: Text("${model['time']}"),
           ),
           const SizedBox(width: 15,),
           Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               Text("${model['title']}", style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-              Text("${model['time']}", style: const TextStyle(color: Colors.grey),),
-              Text("${model['date']}", style: const TextStyle(color: Colors.grey),),
-    
+              Text("${model['date']}", style: const TextStyle(color: Colors.grey),),    
             ],
           )
         ],
