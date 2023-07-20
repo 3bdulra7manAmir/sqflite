90d7ab1ed2481c3321bfc6291b55f31f181be213
3bdulra7manAmir <ethanhunt4d@gmail.com>
2023-07-20 11:39:43 +0300

    Validator has been fixed

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -16,25 +16,26 @@ class HomeLayout extends StatefulWidget {
 }
 
 class _HomeLayoutState extends State<HomeLayout> {
-
-int currentIndex = 0;
-List<Text> appbarText = const [Text("New Tasks"), Text("Done Tasks"), Text("Archived Tasks")];
+  int currentIndex = 0;
+  List<Text> appbarText = const [
+    Text("New Tasks"),
+    Text("Done Tasks"),
+    Text("Archived Tasks")
+  ];
 //List<String> titles = ["New Tasks","Done Tasks","Archived Tasks"];
-List<Widget> screens = const [Tasks(), DoneTasks(), ArchivedTasks()];
+  List<Widget> screens = const [Tasks(), DoneTasks(), ArchivedTasks()];
 
-late Database database;
-var scaffoldKey = GlobalKey<ScaffoldState>();
-var formKey = GlobalKey<FormState>();
-bool isOpened = true;
-bool iconChange = true;
-IconData icon = Icons.edit;
+  late Database database;
+  var scaffoldKey = GlobalKey<ScaffoldState>();
+  var formKey = GlobalKey<FormState>();
+  bool isOpened = true;
+  bool iconChange = true;
+  IconData icon = Icons.edit;
+  var titleController = TextEditingController();
+  var timeController = TextEditingController();
+  var dateController = TextEditingController();
 
-var titleController = TextEditingController();
-var timeController = TextEditingController();
-var dateController = TextEditingController();
-//var 
-
-@override
+  @override
   void initState() {
     createDatabase();
     super.initState();
@@ -51,7 +52,7 @@ var dateController = TextEditingController();
       body: screens[currentIndex],
       floatingActionButton: FloatingActionButton(
         child: Icon(icon),
-        onPressed: (){ 
+        onPressed: () {
           // var name = await printName();
           // print(name);
           // try{
@@ -69,114 +70,132 @@ var dateController = TextEditingController();
           //   print("error $error");
           // });
 
-        //insertToDatabase();
-        if(isOpened){
-          scaffoldKey.currentState?.showBottomSheet((context) => 
-            // Container(
-            //   width: double.infinity,
-            //   height: 120.0,
-            //   color: Colors.red,
-            // )
-            Container(
-              color: Colors.grey[200],
-              padding: const EdgeInsets.all(15),
-              child: Form(
-                key: formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    defaultTextFormField(controller: titleController,
-                    keyboardType: TextInputType.text,
-                    hintText: "Enter Your name Plz",
-                    labelText: "Your name!",
-                    prefixIcon: Icons.title,
-                    onTap: () {print("title tapped");},
-                    ),
-                    const SizedBox(height: 15,),
-                    defaultTextFormField(controller: timeController,
-                    keyboardType: TextInputType.datetime,
-                    labelText: "Task time",
-                    prefixIcon: Icons.watch_later_outlined,
-                    // onTap: () {print("time tapped");},
-                    onTap: () {showTimePicker(context: context, initialTime: TimeOfDay.now())
-                    .then((value){
-                      print(value?.format(context));
-                      var time = value?.format(context);
-                      timeController.text = time.toString();
-                    });
-                    },
+          //insertToDatabase();
+          if (isOpened) {
+            scaffoldKey.currentState?.showBottomSheet(
+              (context) =>
+                  // Container(
+                  //   width: double.infinity,
+                  //   height: 120.0,
+                  //   color: Colors.red,
+                  // )
+                  Container(
+                color: Colors.grey[200],
+                padding: const EdgeInsets.all(15),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      defaultTextFormField(
+                        controller: titleController,
+                        keyboardType: TextInputType.text,
+                        placeHolderText: "Enter Your name Plz",
+                        labelText: "Your name!",
+                        prefixIcon: Icons.title,
+                        onTap: () {
+                          print("title tapped");
+                        }, validate: (value) { 
+                          if(value.toString().isEmpty){return 'Name must not be empty';}
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      defaultTextFormField(
+                        controller: timeController,
+                        keyboardType: TextInputType.datetime,
+                        labelText: "Task time",
+                        prefixIcon: Icons.watch_later_outlined,
+                        // onTap: () {print("time tapped");},
+                        onTap: () {
+                          showTimePicker(
+                                  context: context,
+                                  initialTime: TimeOfDay.now())
+                              .then((value) {
+                            print(value?.format(context));
+                            var time = value?.format(context);
+                            timeController.text = time.toString();
+                          });
+                        }, validate: (value) {},
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      defaultTextFormField(
+                        controller: dateController,
+                        keyboardType: TextInputType.datetime,
+                        labelText: "Task time",
+                        prefixIcon: Icons.calendar_month_outlined,
+                        // onTap: () {print("time tapped");},
+                        onTap: () {
+                          showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime.now(),
+                            lastDate: DateTime.parse("2023-07-19"),
+                          ).then((value) {
+                            var date = DateFormat.yMMM().format(value!);
+                            print(date);
+                            dateController.text = date.toString();
+                          });
+                        }, validate: (value) {  },
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 15,),
-                  defaultTextFormField(controller: dateController,
-                  keyboardType: TextInputType.datetime,
-                  labelText: "Task time",
-                  prefixIcon: Icons.calendar_month_outlined,
-                  // onTap: () {print("time tapped");},
-                  onTap: () {showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime.now(), lastDate: DateTime.parse("2023-07-19"),)
-                  .then((value){
-                    var date = DateFormat.yMMM().format(value!);
-                    print(date);
-                    dateController.text = date.toString();
-                  });
-                  },
-                ),
-              
-                  ],
                 ),
               ),
-            )
-          );
-        setState(() {
-          icon = Icons.remove;
-        });
-        isOpened = false;
-        }
-        else{
-          // scaffoldKey.currentState?.showBottomSheet((context) => 
-          //   Container(
-          //     width: 0,
-          //     height: 0,
-          //   )
-          // );
-          //orrrr
-          insertToDatabase(date: dateController.text,
-          time: timeController.text,
-          title: titleController.text,
-          ).then((value){
-
-          });
-        Navigator.pop(context);
-        setState(() {
-                  icon = Icons.add;
-        });
-        isOpened = true;
-        }
-        
+              elevation: 50,
+            );
+            setState(() {
+              icon = Icons.remove;
+            });
+            isOpened = false;
+          } else {
+            // scaffoldKey.currentState?.showBottomSheet((context) =>
+            //   Container(
+            //     width: 0,
+            //     height: 0,
+            //   )
+            // );
+            //orrrr
+            insertToDatabase(
+              date: dateController.text,
+              time: timeController.text,
+              title: titleController.text,
+            ).then((value) {});
+            Navigator.pop(context);
+            setState(() {
+              icon = Icons.add;
+            });
+            isOpened = true;
+          }
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
-        onTap: (index)
-      {
-        setState(() {
-          currentIndex = index;
-          print(index);
-        });
-      },
-        currentIndex: currentIndex,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.menu_book_rounded),
-          label: "Tasks",),
-
-        BottomNavigationBarItem(
-          icon: Icon(Icons.check_circle_outline),
-          label: "Done",),
-
-        BottomNavigationBarItem(
-          icon: Icon(Icons.archive_rounded),
-          label: "Archived",),
-      ]),
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+              print(index);
+            });
+          },
+          currentIndex: currentIndex,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.menu_book_rounded),
+              label: "Tasks",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.check_circle_outline),
+              label: "Done",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.archive_rounded),
+              label: "Archived",
+            ),
+          ]),
     );
   }
 
@@ -184,42 +203,38 @@ var dateController = TextEditingController();
 //   return "Ahmed Ali";
 // }
 
-void createDatabase() async{
-  database = await openDatabase(
-    "todo.db",
-    version: 1,
-    onCreate: (database, version){
+  void createDatabase() async {
+    database = await openDatabase("todo.db", version: 1,
+        onCreate: (database, version) {
       print("database Created");
-      database.execute('create table tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)')
-      .then((value){
+      database
+          .execute(
+              'create table tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)')
+          .then((value) {
         print("Table Created");
-      }).catchError((error)
-      {
+      }).catchError((error) {
         print("Error While Creating Database: $error");
       });
-  },
-  onOpen: (database){
-    print("database Opened");
-  });
-}
-
-
-Future insertToDatabase({
-required String title,
-required String time,
-required String date,
-}) async{
-  return await database.transaction((txn) async{
-  txn.rawInsert("INSERT INTO tasks(title, date, time, status) VALUES('$title','$time','$date','new')")
-  .then((value){
-      print("$value Inserted Successfully");
-    }).catchError((error){
-      print("Error while Inserting: $error");
+    }, onOpen: (database) {
+      print("database Opened");
     });
-    //throw("error"); //me
-  });
-}
+  }
 
-
-
+  Future insertToDatabase({
+    required String title,
+    required String time,
+    required String date,
+  }) async {
+    return await database.transaction((txn) async {
+      txn
+          .rawInsert(
+              "INSERT INTO tasks(title, date, time, status) VALUES('$title','$time','$date','new')")
+          .then((value) {
+        print("$value Inserted Successfully");
+      }).catchError((error) {
+        print("Error while Inserting: $error");
+      });
+      //throw("error"); //me
+    });
+  }
 }
--- lib/shared/default_form_field.dart
+++ lib/shared/default_form_field.dart
@@ -1,38 +1,39 @@
+// ignore_for_file: avoid_print
 import 'package:flutter/material.dart';
 
 Widget defaultTextFormField({
   required TextEditingController controller,
   required TextInputType keyboardType,
-  //required Function? Function() onFieldSubmitted,
-  Function? Function()? suffixPressed,
-  Function? Function()? onTap,
-  //required Function? Function() validate,
-  //required InputDecoration decoration,
-  String? hintText,
+  String? placeHolderText,
   String? labelText,
+  //required Function? Function() validate,
+  required String? Function(String?) validate,
+  Function? Function()? onFieldSubmitted,
   required IconData prefixIcon,
   IconData? suffix,
+  Function? Function()? suffixPressed,
+  Function? Function()? onTap,
   bool isPassword = false,
   isClickable = true,
+  //InputDecoration? decoration,
 }) =>
 
 TextFormField(
   controller: controller,
   keyboardType: keyboardType,
-  //onFieldSubmitted: (value) {print(value);},
-  //onChanged: ( (value)      {print(value);}),
-  //validator: validate,
+  onFieldSubmitted: (value) {print(value);},
+  //onChanged: ( (value)    {print(value);}),
+  validator: validate,
   obscureText: isPassword,
   onTap: onTap,
   enabled: isClickable,
-  decoration: InputDecoration(
-  hintText: hintText,
-  labelText: labelText,
-  prefixIcon: Icon(prefixIcon),
-  suffixIcon: suffix != null ? IconButton(icon: Icon(suffix), onPressed: () { suffixPressed; },) : null, //or just Icon(suffix) directly
-  border: const OutlineInputBorder(),
-    ),
+
+  decoration: InputDecoration
+  (
+    hintText: placeHolderText,
+    labelText: labelText,
+    prefixIcon: Icon(prefixIcon),
+    suffixIcon: suffix != null ? IconButton(icon: Icon(suffix), onPressed: () { suffixPressed; },) : null, //or just Icon(suffix) directly
+    border: const OutlineInputBorder(),
+  ),
 );
-
-//==========================================================================================//
-
